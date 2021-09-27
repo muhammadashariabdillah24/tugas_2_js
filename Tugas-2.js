01be7606a3279fd5f84383c34657d3a2386668fe
Muhammad Ashari Abdillah <tutordunet09@gmail.com>
2021-09-27 08:10:51 +0700

    tugas_2_js

---
+++ Tugas-2.js
@@ -0,0 +1,15 @@
+let varOne = 10
+let varString = 'Niomic'
+let varBoolean = true
+let varUndefined = undefined
+let varArray = ['One','Two','Three']
+
+console.log(
+    `
+    ${varOne}\n
+    ${varString}\n
+    ${varBoolean}\n
+    ${varUndefined}\n
+    ${varArray}
+    `
+);
